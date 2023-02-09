d59cbc9306558be1cf26a66bfe24a622ef36fd68
aurora32s <aurora32s@naver.com>
2023-02-09 23:41:27 +0900

    [Test] #5 - Long.fullTimeFormat 메서드 중 N일 HH시간 MM분 SS초 차이날 때 테스트 코드 작성

--- app/src/test/java/com/haman/dearme/util/ext/LongExtensionTest.kt
+++ app/src/test/java/com/haman/dearme/util/ext/LongExtensionTest.kt
@@ -4,6 +4,7 @@ import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 class LongExtensionTest {
     @Test
@@ -18,5 +19,23 @@ class LongExtensionTest {
 
         // 3. Then
         assertThat(result, `is`("5일"))
+    }
+
+    @Test
+    fun getFullTimeFormat_Long_returnTimeDuration() {
+        // 1. Given
+        val now = LocalDateTime.of(
+            2023, 2, 10, 20, 10, 35
+        )
+        val date = LocalDateTime.of(
+            2023, 2, 11, 23, 59,59
+        )
+
+        // 2. When
+        val diff = date.getTime() - now.getTime()
+        val result = diff.fullTimeFormat()
+
+        // 3. Then
+        assertThat(result, `is`("1일 3시간 49분 24초"))
     }
 }
