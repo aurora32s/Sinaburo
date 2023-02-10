65ccb0d99391552bc5dc93dd877c62cba2b0c29a
aurora32s <aurora32s@naver.com>
2023-02-10 22:38:48 +0900

    [Test] #10 - YearMonth.fullFormat 테스트 코드 작성(통과)

--- app/src/test/java/com/haman/dearme/util/ext/YearMonthExtensionTest.kt
+++ app/src/test/java/com/haman/dearme/util/ext/YearMonthExtensionTest.kt
@@ -1,4 +1,21 @@
 package com.haman.dearme.util.ext
 
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import java.time.YearMonth
+
 internal class YearMonthExtensionTest {
+
+    @Test
+    fun getFullFormat_YearMonth_returnYYYYMM() {
+        // 1. Given
+        val now = YearMonth.of(2023, 2)
+
+        // 2. When
+        val result = now.fullFormat()
+
+        // 3. Then
+        assertThat(result, `is`("2023년 2월"))
+    }
 }
